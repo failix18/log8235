8fbfada6e91dd7919ee08c8de4e13f0d46f11a91
Vincent <vincent@getting-nutz.fr>
2022-02-06 18:49:31 -0500

    timer

---
+++ TP1/Intermediate/Build/Win64/SoftDesignTrainingEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/vince/Desktop/Cours polymtl/trimestre 1/INF8235/TP1/log8235/TP1/Intermediate/Build/Win64/SoftDesignTrainingEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ TP1/Intermediate/Build/Win64/UE4Editor/Inc/SoftDesignTraining/SDTAIController.generated.h
@@ -0,0 +1,99 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SOFTDESIGNTRAINING_SDTAIController_generated_h
+#error "SDTAIController.generated.h already included, missing '#pragma once' in SDTAIController.h"
+#endif
+#define SOFTDESIGNTRAINING_SDTAIController_generated_h
+
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_SPARSE_DATA
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_RPC_WRAPPERS
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_RPC_WRAPPERS_NO_PURE_DECLS
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASDTAIController(); \
+	friend struct Z_Construct_UClass_ASDTAIController_Statics; \
+public: \
+	DECLARE_CLASS(ASDTAIController, AAIController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SoftDesignTraining"), NO_API) \
+	DECLARE_SERIALIZER(ASDTAIController) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_INCLASS \
+private: \
+	static void StaticRegisterNativesASDTAIController(); \
+	friend struct Z_Construct_UClass_ASDTAIController_Statics; \
+public: \
+	DECLARE_CLASS(ASDTAIController, AAIController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/SoftDesignTraining"), NO_API) \
+	DECLARE_SERIALIZER(ASDTAIController) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASDTAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASDTAIController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASDTAIController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASDTAIController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASDTAIController(ASDTAIController&&); \
+	NO_API ASDTAIController(const ASDTAIController&); \
+public:
+
+
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASDTAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASDTAIController(ASDTAIController&&); \
+	NO_API ASDTAIController(const ASDTAIController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASDTAIController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASDTAIController); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASDTAIController)
+
+
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_PRIVATE_PROPERTY_OFFSET
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_14_PROLOG
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_PRIVATE_PROPERTY_OFFSET \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_SPARSE_DATA \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_RPC_WRAPPERS \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_INCLASS \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TP1_Source_SoftDesignTraining_SDTAIController_h_17_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_PRIVATE_PROPERTY_OFFSET \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_SPARSE_DATA \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_RPC_WRAPPERS_NO_PURE_DECLS \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_INCLASS_NO_PURE_DECLS \
+	TP1_Source_SoftDesignTraining_SDTAIController_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SOFTDESIGNTRAINING_API UClass* StaticClass<class ASDTAIController>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TP1_Source_SoftDesignTraining_SDTAIController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
